248528e1c1e2c162c01473c689390d97ea43eda4
RC_Student_lab <RC_Student_lab@labG9AEB3>
2024-04-12 15:36:23 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Your Name");
+        Console.Write("Name:");
+        String  name = Console.ReadLine();
+        
+        Console.WriteLine("Enter Your Surname");
+        Console.Write("Surname: ");
+        String surname = Console.ReadLine();
+        Console.WriteLine(name.ToLower());
+        Console.WriteLine(surname.ToUpper());
+        Char SurnameFirstLetter = surname[0];
+        Console.WriteLine(SurnameFirstLetter);
+
+        Console.WriteLine(name.ToLower()+ " " + surname.ToUpper());
+        Console.WriteLine(surname);
+
+    }
+}
+
